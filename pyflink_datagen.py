e9ecf1d2668fe912045eabc2ea57624e480423d0
P Rohan Kumar <rohankumar780@gmail.com>
2024-02-12 03:04:35 +0530

    datastream example

--- pyflink_datagen.py
+++ pyflink_datagen.py
@@ -1,10 +1,15 @@
-from pyflink.table import EnvironmentSettings, TableEnvironment
 import ibis
-
+from pyflink.common import Types
+from pyflink.datastream import KeyedProcessFunction, RuntimeContext
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream.state import ValueStateDescriptor
+from pyflink.table import EnvironmentSettings, StreamTableEnvironment
 
 # 1. create a TableEnvironment
+env = StreamExecutionEnvironment.get_execution_environment()
+table_env = StreamTableEnvironment.create(env)
+
 env_settings = EnvironmentSettings.in_streaming_mode()
-table_env = TableEnvironment.create(env_settings)
 
 con = ibis.flink.connect(table_env)
 
@@ -39,7 +44,6 @@ t = con.create_table(
     ),
 )
 
-
 # 3. query from source table and perform calculations
 agged = t.select(
     province_id=t.provinceId,
@@ -51,5 +55,31 @@ agged = t.select(
 )
 
 
-df = agged.execute()
-print(df)
+class MyProcessFunction(KeyedProcessFunction):
+
+    def __init__(self):
+        self.seen = None
+
+    def open(self, runtime_context: RuntimeContext):
+        state_descriptor = ValueStateDescriptor("seen", Types.INT())
+        self.seen = runtime_context.get_state(state_descriptor)
+
+    def process_element(self, value, ctx):
+        provinceId = value[0]
+        if self.seen.value() is None:
+            self.seen.update(provinceId)
+            yield provinceId
+
+
+sql = con.compile(agged.as_table())
+
+res_ds = table_env.to_data_stream(table_env.sql_query(sql))
+
+res_ds.key_by(lambda r: r[0]) \
+    .process(MyProcessFunction()) \
+    .print()
+
+env.execute()
+
+# df = agged.execute()
+# print(agged.to_pandas())
